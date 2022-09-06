75bbc1bd0aad443cafab5ec3d4bfeab3646ab22f
JeHyuckLee <20171593@edu.hanbat.ac.kr>
2022-09-06 13:13:09 +0900

    9.6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {Button,Form, Navbar, Container,Nav} from 'react-bootstrap';
 import './App.css';
+import data from "./data";
+
+
 
 function App() {
+
+  let[shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Market</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'>
+      </div>
+
+      <div className='container'>
+
+        
+        <div className='row'>
+          <div className='col-md-4'>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="shoes" width="70%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+          <div className='col-md-4'>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="shoes" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+          <div className='col-md-4'>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="shoes" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+function shoes(props){
+  let url = `https://codingapple1.github.io/shop/shoes${index+1}.jpg`
+  return(
+    <div className='col-md-4'>
+    <img src={url} alt="shoes" width="80%"/>
+    <h4>item[index].title</h4>
+    <p>item[index].price</p>
     </div>
   );
 }
